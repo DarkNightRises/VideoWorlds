1e63de7db4ff54cdb8cbdc0b490ac85225b292e6
KKS161994 <kartikeyshrivastava@gmail.com>
2017-03-20 03:03:53 +0530

    Added tabs shots profile short tab

--- src/ProfileView/BodyItems.js
+++ src/ProfileView/BodyItems.js
@@ -3,13 +3,20 @@ import './CSS/styles.css';
 import { DefaultPlayer as Video } from 'react-html5video';
 import resumevideo from './Images/resume-pro.mp4';
 import profile_image from './Images/pro.jpg';
+import post_profile_pic from './Images/pro3.jpg';
+import shots_pic from './Images/resume-cover.jpg';
+import shots_pic_next from './Images/resume-pro-cover.jpg';
 class BodyItems extends Component{
 	constructor(props){
 		super(props);
 	}
 	render(){
 		var listName = ["Followers","Views","Likes"];
-		var listNumbers = [325,2462,2493];;
+		var listNumbers = [325,2462,2493];
+		var shotsNames = ["Mag Cover","Crown Queen","Simply best"];
+		var shotsDays = ["2 days ago","27 Mar.","12 Feb"];
+		var shotsImages = [shots_pic,shots_pic_next,shots_pic];
+		var tabsTitles=["INTRO","SHOTS","IDENTITY"]; 
 		var videoIconsDiv = <div className="videoDivIcons">
 		<div className="row">
 		<div className="col-xs-12 col-sm-7" />
@@ -51,28 +58,85 @@ class BodyItems extends Component{
 
 
 		var lastActiveText = <div className="lgttext">Last Active: 2 hrs ago</div>
-		var timeLine = <section className="timeLine">
-		<div className="container">
-		<div className="row">
-		<div className="col-xs-12 col-sm-4">
+		var tagLineText = <div className="tagline">The secret of getting ahead is getting started.</div>
+		var moreInfo = <div className="moreInfoBtn">
+		<i className="fa fa-angle-down moreInfoi" />
+		</div>;
+		var timeLine = <div className="col-xs-12 col-sm-4">
 		<div className="white-content clearfix top-intro">
 		{pro_profile}
 		<NumbersList numbers={listNumbers} titles={listName}/>
 		{lastActiveText}
+		{tagLineText}
+		{moreInfo}
 		</div>
+		<ClearFix />
+		<div className="white-content pd510 clearfix hidden-xs">
+		  <h2>Shots  
+		  <span className="titlecount">
+		  25
+		  </span>
+		  </h2>
+
+		  <ClearFix />
+		  <ShotsList names={shotsNames} days={shotsDays} images={shotsImages} />
+		  <a href="#" className="vmtag transition">
+		  View more
+		  </a>
 		</div>
-		<div className="col-xs-12 col-sm-8">
+		</div>;
 		
+		var tabsDiv = <div className="white-content pd10 mrtp20">
+		<div className="row">
+		<div className="col-xs-12 col-sm-12">
+		<div className="videotab">
+		<div className="vtab w25 active">RECENT</div>
+		<Tabs names={tabsTitles} />
 		</div>
 		</div>
 		</div>
-		</section>;
-		
+		</div>
+
+		var postDiv = <div className="col-xs-12 col-sm-8 postDiv">
+		<div className="row">
+		<div className="col-xs-12 col-sm-12">
+		<div className="tprc">
+		<img src={post_profile_pic} className="usrcmtimg"/>
+		<textarea title="Leave a post for shweta..." 
+		name="tprc" placeholder="Leave a post for shweta..."  className="tprcinput" />
+        <div className="procmtbtn clearfix">
+        <span className="procmtfa">
+        <i className="fa fa-stack-exchange" />
+        </span>
+        <span className="procmtfa">
+        <i className="fa fa-video-camera" /></span>
+        <button type="button" className="btn btn-shrofile red pull-right">Post</button>
+        </div>     
+		</div>
+		</div>
+		</div>
+		{tabsDiv}
+		<ClearFix />
+		</div>
 
 
+
+		var timeLineRoot= <section className="timeLine">
+		<div className="container">
+		<div className="row">
+		{timeLine}
+		{postDiv}
+
+
+
+
+		</div>
+		</div>
+		</section>
 		return <div>
 		{videoDiv}
-		{timeLine}
+		{timeLineRoot}
+		
 		</div>;
 	}
 }
@@ -86,9 +150,34 @@ function NumbersList(props){
 			{titles[index]}
 			
 			<span className="pro-list-elem-span">{number}</span></a>
+			
 			</li>
 		);
 	return <ul className="pro-list">{listItems}</ul>
+}
+function ShotsList(props){
+	const names = props.names;
+	const days = props.days;
+	const images = props.images;
+	const listItems = names.map((name,index)=>
+		<div><a href="#" className="shotsbox transition">
+		<img src={images[index]} alt />
+		<h5>{name}</h5>
+		<span className="cmttime">{days[index]}</span></a>
+		<div className="lgt-separator"></div></div>
+		);
+	return <div>{listItems}</div>;
+}
+
+function Tabs(props){
+	const names = props.names;
+	const listItems = names.map((name)=>
+		<div className="vtab w25">{name}</div>
+		);
+	return <div>{listItems}</div>;
+}
+function ClearFix(){
+	return <div className="clearfix"></div>
 }
 
 export default BodyItems;
